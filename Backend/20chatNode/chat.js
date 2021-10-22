60c8a000c14153935d61e50808947685bd0a5545
Israelí Pérez <israeli.ps@gamil.com>
2021-10-22 00:30:07 -0500

    update

---
+++ Backend/20chatNode/chat.js
@@ -0,0 +1,52 @@
+const path=require('path');
+const express=require('express');
+const serverHttp=require('http');
+const socket=require('socket.io');
+
+const app  = express();
+const http = serverHttp.Server(app);
+const io   = socket(http);
+const port=8000
+
+// app.set('port',process.env.PORT,8000)
+let dir=path.join(__dirname,'public')
+
+app.use(express.static(dir))
+
+io.on('connection', function(socket) {
+    console.log('New user connected');
+
+    socket.on('nuevo mensaje', function(msj) {
+        io.emit('nuevo mensaje', msj);
+    });
+    
+    socket.on('chat:message',(data)=>{
+        io.emit('chat:message', data);
+    })
+
+    socket.on('chat:typing',(data)=>{
+        socket.broadcast.emit('chat:typing',data)
+    })
+    
+    socket.on('disconnect', function() {
+        console.log('Usuario desconectado');
+    });
+});
+
+
+
+
+
+
+
+
+
+// app.get('/', function(req, res) {
+//     res.sendFile( __dirname + '/chat.html');
+// });
+
+
+
+http.listen(port, ()=> {
+    console.log(`listening on *:${port}`);
+});
